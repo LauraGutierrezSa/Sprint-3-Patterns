7fca0000eae0c44c0e310d592cf6a8f030fa178b
LauraGutierrezSa <117345464+LauraGutierrezSa@users.noreply.github.com>
2023-02-22 09:19:26 +0100

    Update fixed_callback_hell.js

--- callback hell/fixed_callback_hell.js
+++ callback hell/fixed_callback_hell.js
@@ -1,18 +1,25 @@
-const { readdir, readFile, writeFile} = require("fs");
+const { readdir, readFile, writeFile, write } = require("fs");
 const { join } = require("path");
 const inbox = join(__dirname, "inbox");
 const outbox = join(__dirname, "outbox");
 
-const readdir(inbox, (error, files) => {
-    if (error) return console.log("Error: Folder inaccessible")
-});
+const reverseText = (str) => str.split("").reverse().join("");
 
-const readAndWrite = readFile(join(inbox, file), "utf8", (error, data) => {
-    if (error) return console.log("Error: File error");
-    writefn(join(outbox, file))
-});
+const writeFiles = (file, data) => {
+    writeFile(join(outbox, file), reverseText(data), (error) => {
+        if (error) return console.log("Error: File could not be saved!");
+        console.log(`${file} was successfully saved in the outbox!`);
+    });
+};
 
-const writefn = reverseText(data), error => {
-    if (error) return console.log("Error: File could not be saved!");
-    console.log(`${file} was successfully saved in the outbox!`);
-  };
+const readAndWrite = (file) => {
+    readFile(join(inbox, file), "utf8", (error, data) => {
+        if (error) return console.log("Error: File error");
+        writeFiles(file, data);
+    });
+};
+
+readdir(inbox, (error, files) => {
+  if (error) return console.log("Error: Folder inaccessible");
+  files.forEach((file) => readAndWrite(file));
+});
